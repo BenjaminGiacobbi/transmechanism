0eb6d23b5e28d4b296e9e59fa71da40a90a87086
Benjamin Giacobbi <benjamingiacobbi@gmail.com>
2021-02-12 19:18:28 -0600

    Whiteboxing Pt 1
    Most areas at least boxed out, traversable

---
+++ Assets/Editor/AssignMaterials.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AssignMaterial : ScriptableWizard
+{
+    public Material _material;
+    GameObject[] gos;
+
+    void OnWizardUpdate()
+    {
+        helpString = "Select Game Obects";
+        isValid = (_material != null);
+    }
+
+    void OnWizardCreate()
+    {
+
+        gos = Selection.gameObjects;
+        foreach (var go in gos)
+        {
+            go.GetComponent<Renderer>().material = _material;
+        }
+    }
+
+    [MenuItem("Custom/Assign Material", false, 4)]
+    static void SetMaterial()
+    {
+        DisplayWizard("Assign Material", typeof(AssignMaterial), "Assign");
+    }
+}
